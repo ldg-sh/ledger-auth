8f352300cf1655ee45f33b858012842103f3a033
Noah <me@noahdunnagan.com>
2025-08-30 22:58:48 -0400

    feat: submodule protobuf & pullall makefile alias

--- build.rs
+++ build.rs
@@ -1,5 +1,12 @@
+use tonic_prost_build::configure;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=proto/ledger.proto");
-    tonic_prost_build::compile_protos("proto/ledger.proto")?;
+    configure()
+        .compile_protos(
+            &[
+                "proto/auth/auth.proto"
+            ],
+            &["proto"],
+        )?;
     Ok(())
 }
